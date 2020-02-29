25fecf35e3bcb08d3b9aa7998d6b97951e64b31b
Divyaraj Jadeja <jadejadivyaraj@gmail.com>
2020-02-29 13:26:26 +0100

    IHttpMaxRequestBodySizeFeature ENABLED

--- ImageProcessing.Web/Startup.cs
+++ ImageProcessing.Web/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -41,6 +42,14 @@ namespace ImageProcessing.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.Use(async (context, next) =>
+            {
+                context.Features.Get<IHttpMaxRequestBodySizeFeature>()
+                    .MaxRequestBodySize = null;
+
+                await next.Invoke();
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
